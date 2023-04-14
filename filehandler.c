e7f0449bbd9a93a981b0d5d0b0d5f811e53379b0
Asbjørn <s204026@student.dtu.dk>
2023-04-14 11:58:08 +0200

    Changes by nth

--- filehandler.c
+++ filehandler.c
@@ -3,22 +3,27 @@
 //
 #include "filehandler.h"
 
-
 Card *deckFromFile(char *filename){
+    Card *deck;
     FILE *file = NULL;
+    char *path = "C:\\Users\\asbpo\\CLionProjects\\02322Projekt2-Yukon\\new.txt";
     char str[3];
-    file = fopen ("new.txt", "r");
+    file = fopen (path, "r");
     if(file == NULL){
         printf("error");
         return NULL;
     }
 
     while (fgets(str, 2, file) != NULL){
-        printf("%s", str);
+        printf(&str[0]);
+        printf(&str[1]);
+
+        addCard(&deck, str[1], str[0]);
     }
 
     // Closing the file
     fclose(file);
+    return deck;
 }
 
 Card *newDeck(){
--- filehandler.h
+++ filehandler.h
@@ -6,6 +6,7 @@
 #define INC_02322PROJEKT2_YUKON_FILEHANDLER_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "deck.h"
 
 Card *deckFromFile(char *filename);

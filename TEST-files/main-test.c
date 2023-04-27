c17dbc1160b6dbc92d5805d41e41c926a7c2451a
Philip Astrup Cramer <s224319@dtu.dk>
2023-04-27 11:13:51 +0200

    added board test

--- TEST-files/main-test.c
+++ TEST-files/main-test.c
@@ -4,10 +4,15 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include "column-test.h"
+#include "board-test.h"
 
 int main(){
 
-    printf("Column test result: %d", columnTest());
+    printf("Column test result: %d\n", columnTest());
+    printf("============================= BOARD TEST TOP =============================\n");
+    testPrintboard();
+    testPrintGameConsole();
+    printf("\n============================= BOARD TEST END =============================\n");
 
     return 0;
 }

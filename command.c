044d6368c79be4ea8c029b1061aa3c4d0728752a
Asbjørn <s204026@student.dtu.dk>
2023-04-29 13:04:40 +0200

    Creating command cards should be down through the playCommand(). Doing it this way should ensure that false commands are kept to a minimum.
    Bit unsure if it should return void or command. It could be nice if it returned command, because then we could instantly validate if the command was done and pass it to the status message.

--- command.c
+++ command.c
@@ -5,6 +5,11 @@
 
 #include <malloc.h>
 #include "command.h"
+#include "column.h"
+#include "stdio.h"
+#include "string.h"
+#include "board.h"
+
 
 void doCommand(char *moveTo, char *moveFrom, char *card){
     command *cmd;
@@ -16,4 +21,35 @@ void doCommand(char *moveTo, char *moveFrom, char *card){
     cmd->prevCommand = NULL;
 };
 
-
+void playCommand(Board *board,char *str){
+    char *tmp = strdup(str);
+    if(strcmp(&str[2],":") == 0) {
+        char *coulFro = strtok(tmp, ":");
+        char *card = strtok(NULL,":");
+        char *coulTo = strtok(NULL, "");
+        doCommand(coulTo, coulFro, card);
+        free(tmp);
+    }
+    else if(strcmp(&str[2], "-") == 0){
+        char *coulFro = strtok(tmp,"-");
+        char *coulTo = strtok(NULL,"-");
+        char *card = NULL;
+        for(int i = 0; i < board->column; i++){
+            if(coulFro[1] == i) {
+                char *tmp1 = &board->column[i].tail->order;
+                char *tmp2 = &board->column[i].tail->suit;
+                strcat(card,tmp1);
+                strcat(card,tmp2);
+                break;
+            }
+        }
+        doCommand(coulTo,coulFro,card);
+        }
+    else if(strcmp(str,"UNDO") == 0 || strcmp(str,"undo") == 0){
+        //Should return NULL for now.
+    }else if(strcmp(str, "REDO") == 0 || strcmp(str,"redo") == 0){
+        //Should return NULL for now.
+    }else{
+        //Should return NULL has it is not a valid command
+    }
+}

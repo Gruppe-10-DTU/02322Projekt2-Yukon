2712e35bc4c2b0d3198907f64c4c32ec1c27e6f5
Asbjørn <s204026@student.dtu.dk>
2023-05-09 16:28:46 +0200

    Use realloc inside while loop instead. Should try to reallocate the resources already allocated to moveCmd.

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ void startGame(Board* board) {
         //Used for debug only. Wouldn't print console output when debugging, found this solution online.
         setbuf(stdout, 0);
         printGameConsole(lastMove, status);
-        moveCmd = (char*) malloc(sizeof moveCmd);
+        moveCmd = (char*) realloc(moveCmd, sizeof moveCmd);
         scanf("%s", moveCmd);
         if (strcasecmp(moveCmd, "Q") == 0) {
             strcpy(status, "Exiting game...");
